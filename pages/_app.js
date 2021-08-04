4b550c2c4299086be557e97782c89f5b98793f2e
Joebless <ahialad2@gmail.com>
2021-08-04 23:16:40 +0100

    conected

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+import { AuthProvider } from "../lib/auth";
+const App = ({ Component, pageProps }) => {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+};
+export default App;
