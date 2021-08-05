073c2f715b6657639d625e667948308f810a811a
Joebless <ahialad2@gmail.com>
2021-08-05 10:41:18 +0100

    main

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import { AuthProvider } from "../lib/auth";
+import theme from "../styles/theme";
 const App = ({ Component, pageProps }) => {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <CSSReset />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
   );
 };
 export default App;
--- pages/p/[siteId].js
+++ pages/p/[siteId].js
@@ -1,1 +1,30 @@
 import { Box } from "@chakra-ui/core";
+import { useRouter } from "next/router";
+import { getAllFeedback, getAllSites } from "../../lib/db-admin";
+export async function getStaticProps(context) {
+  const siteId = context.params.siteId;
+  const { feedback } = await getAllFeedback(siteId);
+  return {
+    props: {
+      initialFeedback: feedback,
+    },
+    revalidate: 1,
+  };
+}
+export async function getStaticPaths() {
+  const { sites } = await getAllSites();
+  const paths = sites.map((site) => ({
+    params: {
+      siteId: site.id.toString(),
+    },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+const FeedbackPage = ({ initialFeedback }) => {
+  const router = useRouter();
+  return <Box>Site ID: ${router.query.siteId}</Box>;
+};
+export default FeedbackPage;
